c19b7e849b6c11aa0220bf6cd78b22e684927b03
Mainak Deb <debmainak37@gmail.com>
2021-10-04 01:30:05 +0530

    plug board added here

--- enigma.js
+++ enigma.js
@@ -2,6 +2,7 @@ let o="",r="",p="",t="";
 
 
 function Enigma_code(txt,orientation,rotor,plug_board){
+    console.log(plug_board)
     let rotor_box=[[],[],[]]
     let rotor_i=[21, 5, 9, 18, 14, 11, 0, 15, 23, 3, 1, 16, 22, 20, 17, 7, 4, 6, 8, 25, 13, 10, 24, 19, 2, 12];
     let rotor_ii=[23, 8, 14, 17, 25, 2, 15, 11, 22, 16, 10, 9, 12, 21, 3, 19, 6, 13, 5, 4, 18, 1, 24, 20, 7, 0];
@@ -38,36 +39,36 @@ function Enigma_code(txt,orientation,rotor,plug_board){
         }
     }
 
-    console.log(rotor_box)
-    console.log(txt,txt.length)
+    // console.log(rotor_box)
+    // console.log(txt,txt.length)
     for(let i=0;i<txt.length;i++){
         //step 1
         if(txt[i]!=' '){
 
-            console.log(i,txt[i],to_ascii(txt[i]))
-            let s_input=to_ascii(txt[i]);console.log("s_input",s_input)
-            let s1=rotor_box[0][s_input];console.log("s1",s1)
-            let s2=rotor_box[1][s1];console.log("s2",s2)
-            let s3=rotor_box[2][s2];console.log("s3",s3)
+            //console.log(i,txt[i],to_ascii(txt[i]))
+            let s_input= plug_board[to_ascii(txt[i])]//console.log("s_input",s_input)
+            let s1=rotor_box[0][s_input];//console.log("s1",s1)
+            let s2=rotor_box[1][s1];//console.log("s2",s2)
+            let s3=rotor_box[2][s2];//console.log("s3",s3)
 
             let r=reflector[s3]
 
-            let r1=rotor_box[2].indexOf(r);console.log("r1",r1);
-            let r2=rotor_box[1].indexOf(r1);console.log("r2",r2);
-            let r3=rotor_box[0].indexOf(r2);console.log("r3",r3);
+            let r1=rotor_box[2].indexOf(r);//console.log("r1",r1);
+            let r2=rotor_box[1].indexOf(r1);//console.log("r2",r2);
+            let r3=rotor_box[0].indexOf(r2);//console.log("r3",r3);
 
-            let charval=String.fromCharCode(65+r3)
+            let charval=String.fromCharCode(65+ plug_board[r3])
 
             cipher_text=cipher_text+charval;
 
             control_meter()
-            console.log(meter)
+            //console.log(meter)
         }else{
             cipher_text=cipher_text+' ';
         }
 
     }
-    console.log(cipher_text)
+    //console.log(cipher_text)
     return cipher_text;
 
 }
@@ -88,6 +89,20 @@ function copy_to_clipboard(){
   alert("Copied the text: " + copyText.value);
 }
   
+function plug_board(s){
+    a=[]
+    for(let i=0;i<26;i++){a.push(i)}
+    console.log(a)
+    for(let i=0;i<s.length;i++){
+        let ac=s[i];
+        a[to_ascii(ac[0])]=to_ascii(ac[1]);
+        a[to_ascii(ac[1])]=to_ascii(ac[0]);
+    }
+    console.log(a)
+    return a;
+}
+
+
 
 function takeinput(){       
     o=document.getElementById("orientation").value.split("")
@@ -98,17 +113,17 @@ function takeinput(){
 
     o=o.map(to_int);
     r=r.map(to_ascii)
-
+    let alphabet_comb=[];for(let i=0;i<26;i++){alphabet_comb.push(i)}
     if(o.length==0){o=[0,1,2];document.getElementById("orientation").value="123"}
     if(r.length==0){r=[0,1,2];document.getElementById("rotate").value="ABC"}
+    if(p.length!=1){alphabet_comb=plug_board(p)}
+    
 
    
 
-
-    console.log(document.getElementById("cipher"))
     console.log(o,r,p)
     console.log(o.length,r.length,p.length)
-    let ans=Enigma_code(t,o,r,p);
+    let ans=Enigma_code(t,o,r,alphabet_comb);
     document.getElementById("cipher").value=ans;
 
 }
